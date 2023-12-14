433d4716919aba441fd063168f2925c0f5106b97
JPMetoyer <metoyerjeanpaul@gmail.com>
2023-12-13 20:49:26 -0600

    implemented home page design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,80 @@
-
-
-
 <script lang="ts">
+
+    import Coursecard from "$lib/cards/coursecard.svelte";
+    import Lessoncard from "$lib/cards/lessoncard.svelte";
 
 </script>
 
 <main>
-    <h1>Hello World</h1>
 
 
-    <div class="grid">
-        {#each Array(5) as _ }
-        <p>Test</p>
-        {/each }
-    </div>
-    
+    <section id="lesson">
+        <h4>Jump Back in. </h4>
 
+
+        <div class="grid">
+            {#each Array(3) as _ }
+            <Lessoncard/>
+            {/each }
+        </div>
+
+
+    </section>
+
+
+    <section id="course">
+
+        <h4>Explore more courses. </h4>
+
+        <div class="grid">
+            {#each Array(4) as _ }
+            <Coursecard/>
+            {/each }
+        </div>
+
+    </section>
 
 </main>
+
 
 <style lang="scss">
     @use "$lib/interface/variables" as app;
 
-    div.grid {
-        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    main {
+        margin-top: 4rem;
+        margin-left: 5vw;
+        margin-right: 5vw;
+        min-height: 90vh;
     }
 
-    h1 {
-        color: app.$color-brand;
-    }
+    div.grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        gap: 1rem;
+
+     }
+
+     h4 {
+        font-size: 26px;
+        font-weight: bolder;
+        margin-bottom: 15px;
+     }
+
+
+
+
+
+  
+
+               
+
+
 </style>
+
+
+
+
+
+
+
 
